725ffc5a0d1640e2664e98c8619bd94fe34eb973
Kosta Georgiev <kosta.georgiev@dreamix.eu>
2023-07-11 23:06:55 +0300

    fixed bug when update customers

--- Backend/Infrastructure/Services/CustomerService.cs
+++ Backend/Infrastructure/Services/CustomerService.cs
@@ -37,7 +37,7 @@ namespace Infrastructure.Services
             }
             else
             {
-                customer = await _context.Customers.SingleAsync(x => x.Id == customerModel.Id);
+                customer = await _context.Customers.Include(x=>x.Address).SingleAsync(x => x.Id == customerModel.Id);
                 customer.FirstName = customerModel.FirstName;
                 customer.LastName = customerModel.LastName;
                 customer.Email = customerModel.Email;
